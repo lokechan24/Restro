3c175e12f67ee1cefd25ca87fa05f1055c6c154f
lokechan24 <gklokeshwaran@gmail.com>
2024-08-13 18:36:30 +0530

    Your commit message

---
+++ components/footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>locations</h3>
+            <a href="#">india</a>
+            <a href="#">japan</a>
+            <a href="#">USA</a>
+            <a href="#">australia</a>
+            <a href="#">south africa</a>    
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="menu.php">menu</a>
+            <a href="review.php">review</a>  
+            <a href="order.php">order</a>    
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+91 8870609640</a>
+            <a href="#">+91 6385674635</a>
+            <a href="#">lokeshwaran.it22@bitsathy.ac.in</a>
+            <a href="#">kaviyapreethi.cs22@bitsathy.ac.in</a>
+            <a href="#">sathyamangalam, erode</a>    
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#">facebook</a>
+            <a href="#">instagram</a>
+            <a href="#">twitter</a>    
+        </div>
+
+    </div>
+
+<div class="credit"> copyrights @ 2023 by <span> Mr.Cloud </span> </div>
+
+</section>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
